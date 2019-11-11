33caa121cabfb7ac3e13e9df0be93304aec7b2c2
G00359991 <36443124+G00359991@users.noreply.github.com>
2019-11-11 13:06:38 +0000

    Initial commit

---
+++ src/ie/gmit/studentmanager/Main.java
@@ -0,0 +1,12 @@
+package ie.gmit.studentmanager;
+
+public class Main {
+    
+    public static void main(String[] args){
+        // Create a new menu object
+    	Menu menuObject1 = new Menu();
+    	// Run the start method of the Menu object 
+    	menuObject1.display();
+    	
+    }
+}
